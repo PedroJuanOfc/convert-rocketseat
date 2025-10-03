4e55fbce86c40b9b0198c9c0c0ca85e97ca6606c
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-10-03 00:50:35 -0300

    Recuperando a moeda selecionada pelo usuário

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 // Manipulando o input amount para receber apenas números
-amount.addEventListener("input", () => {
+amount.addEventListener("input", (event) => {
   const hasCharactersRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  console.log(currency.value);
+};
