270633b7bab03f0a97f1b65bde78fcaae54f2339
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-10-03 01:03:22 -0300

    Aplicando a classe 'show-result' no footer para exibir o resultado

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const GBP = 7.18;
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 // Manipulando o input amount para receber apenas números
 amount.addEventListener("input", (event) => {
@@ -30,4 +31,14 @@ form.onsubmit = (event) => {
 };
 
 // Função para converter a moeda
-function converterCurrency(amount, price, symbol) {}
+function converterCurrency(amount, price, symbol) {
+  try {
+    // Aplica a classe que exibe o footer para mostrar o resultado
+    footer.classList.add("show-result");
+  } catch (error) {
+    // Remove a classe footer
+    footer.classList.remove("show-result");
+    console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde");
+  }
+}
